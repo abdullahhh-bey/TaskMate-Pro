1bb31b2919a81aac991ad08a1b449d6487d05b37
abalex-ui <kibgheeo775533@gmail.com>
2025-09-03 22:43:12 +0500

    Add project files.

---
+++ TaskMate/Presentation/AuthUserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMate.Presentation
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthUserController : ControllerBase
+    {
+    }
+}
