7dd9a049a509939bb353f066989c89bfedc73c24
abalex-ui <kibgheeo775533@gmail.com>
2025-09-04 23:19:00 +0500

    Made refresh token database w implementation

--- TaskMate/Migrations/20250904181624_AddedRefreshTokenTable.Designer.cs
+++ TaskMate/Migrations/20250904181624_AddedRefreshTokenTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskMate.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using TaskMate.Infrastructure.Data;
 namespace TaskMate.Migrations
 {
     [DbContext(typeof(TaskProDbContext))]
-    partial class TaskProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250904181624_AddedRefreshTokenTable")]
+    partial class AddedRefreshTokenTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
